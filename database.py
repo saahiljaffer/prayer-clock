e77321d26864081048bed75d39f46d9dcb8408e0
Saahil Jaffer <saahil.jaffer@uwaterloo.ca>
2021-01-01 18:21:07 -0500

    Changes afternoon prayers to 24h

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ from datetime import timedelta
 conn = sqlite3.connect('prayertimes.db')
 print ("Opened database successfully")
 
-prayers = ["imsak", "fajr", "sunrise", "zuhr", "sunset", "maghrib"]
+prayers = ["zuhr", "sunset", "maghrib"]
 
 mt = 1
 dt = 1
@@ -18,11 +18,11 @@ for month in range(1, 13):
         for prayer in prayers:
             command = "SELECT " + prayer + " from times WHERE month = " + str(mt) + " AND day = " + str(dt)
             cursor = conn.execute(command)
-            # hour = timedelta(hours=+1)
+            hour = timedelta(hours=+12)
             for row in cursor:
                 date_time_obj = datetime.datetime.strptime(row[0], '%I:%M')
-                date_time_obj = date_time_obj #+ hour
-                new_time = date_time_obj.strftime('%I:%M')
+                date_time_obj = date_time_obj + hour
+                new_time = date_time_obj.strftime('%H:%M')
                 updateCommand = "UPDATE times SET " + prayer + " = '" + new_time + "' WHERE month = " + str(mt) + " AND day = " + str(dt)
                 print(updateCommand)
                 conn.execute(updateCommand)
