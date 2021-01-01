6d023797dcaac0fd4f1c75c630debd41138d11ad
Saahil Jaffer <saahil.jaffer@uwaterloo.ca>
2021-01-01 16:32:25 -0500

    Create play script Broadcasts to google home

--- play.py
+++ play.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+import requests
+import json
+
+url = 'http://localhost/broadcast'
+
+body = {
+    "command": "hello world",
+    "broadcast": 1,
+    "user": "saahil"
+}
+
+response = requests.post(url, json.dumps(body))
+
+print(response)
