c20fd27c6416b89d80092e55f8642679b6c52dc5
Saahil Jaffer <saahil.jaffer@uwaterloo.ca>
2021-01-01 16:19:40 -0500

    Updates database Add leap day and fix formatting

--- database.py
+++ database.py
@@ -10,18 +10,18 @@ print ("Opened database successfully")
 
 prayers = ["imsak", "fajr", "sunrise", "zuhr", "sunset", "maghrib"]
 
-mt = 4
+mt = 1
 dt = 1
 
-for month in range(4, 13):
+for month in range(1, 13):
     for day in range(1, 32):
         for prayer in prayers:
             command = "SELECT " + prayer + " from times WHERE month = " + str(mt) + " AND day = " + str(dt)
             cursor = conn.execute(command)
-            hour = timedelta(hours=+1)
+            # hour = timedelta(hours=+1)
             for row in cursor:
                 date_time_obj = datetime.datetime.strptime(row[0], '%I:%M')
-                date_time_obj = date_time_obj + hour
+                date_time_obj = date_time_obj #+ hour
                 new_time = date_time_obj.strftime('%I:%M')
                 updateCommand = "UPDATE times SET " + prayer + " = '" + new_time + "' WHERE month = " + str(mt) + " AND day = " + str(dt)
                 print(updateCommand)
